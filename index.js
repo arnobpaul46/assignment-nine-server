30839c25b904359a60a5c9170ac513202761d38a
arnobpaul46 <arnobpaulbd@gmail.com>
2026-05-22 12:54:06 +0600

    all doctors data successfully import

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const dotenv =require('dotenv');
-const cors =require('cors');
+const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 const app = express();
 app.use(cors());
-const port =process.env.PORT||8000;
+const port = process.env.PORT || 8000;
+
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const db = client.db("docappoint");
+    const allDoctorsCollection = db.collection('all-doctors')
+    // all doctors list
+    app.get("/all-doctors", async (req, res) => {
+      try {
+        
+        const result = await allDoctorsCollection.findOne({});
+
+        if (result && result.doctors) {
+          res.send(result.doctors); 
+        } else {
+          res.send([]); 
+        }
+      } catch (error) {
+        res.status(500).send("Server Error");
+      }
+
+    })
+
+    
+
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
